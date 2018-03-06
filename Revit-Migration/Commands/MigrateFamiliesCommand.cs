d094211372d77600d315ae4acb3ffc2408ff3856
dklassen <d.p.klassen@gmail.com>
2018-03-06 13:05:07 -0800

    Added code to select families from source directory

--- Revit-Migration/Commands/MigrateFamiliesCommand.cs
+++ Revit-Migration/Commands/MigrateFamiliesCommand.cs
@@ -15,20 +15,24 @@ namespace DougKlassen.Revit.Migration.Commands
     public class MigrateFamiliesCommand : IExternalCommand
     {
         private MigrationLog log = MigrationLog.Instance;
-        String workingDirectory;
+        private String sourceDirectory, destinationDirectory;
+        private List<RevitFamily> familiesToMigrate;
 
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            FolderBrowserDialog startingDirectoryDialog = new FolderBrowserDialog();
-            startingDirectoryDialog.Description = "Select a directory containing families to be migrated";
-            startingDirectoryDialog.ShowNewFolderButton = false;
-            DialogResult result = startingDirectoryDialog.ShowDialog();
+            log.AppendLine("Family Migration: {0}", DateTime.Now.ToLongDateString());
 
+            //Prompt the user for a directory containing files to be migrated
+            DialogResult result;
+            FolderBrowserDialog sourceDirectoryDialog = new FolderBrowserDialog();
+            sourceDirectoryDialog.Description = "Select a directory containing families to be migrated";
+            sourceDirectoryDialog.ShowNewFolderButton = false;
+            result = sourceDirectoryDialog.ShowDialog();
             switch (result)
             {
                 //Directory selected by user
                 case DialogResult.OK:
-                    workingDirectory = startingDirectoryDialog.SelectedPath;
+                    sourceDirectory = sourceDirectoryDialog.SelectedPath;
                     break;
                 //Dialog cancelled
                 case DialogResult.Cancel:
@@ -36,15 +40,57 @@ namespace DougKlassen.Revit.Migration.Commands
                 //Unexpected result
                 default:
                     return Result.Failed;
-                    break;
             }
+            log.AppendLine("Source directory selected: {0}", sourceDirectory);
 
-            log.AppendLine("Family Migration: {0}", DateTime.Now.ToLongDateString());
-            log.AppendLine("Directory selected: {0}", workingDirectory);
+            //Prompt the user for a directory to send migrated files to
+            FolderBrowserDialog destinationDirectoryDialog = new FolderBrowserDialog();
+            destinationDirectoryDialog.Description = "Select an output directory";
+            destinationDirectoryDialog.ShowNewFolderButton = true;
+            result = destinationDirectoryDialog.ShowDialog();
+            switch (result)
+            {
+                //Directory selected by user
+                case DialogResult.OK:
+                    destinationDirectory = destinationDirectoryDialog.SelectedPath;
+                    break;
+                //Dialog cancelled
+                case DialogResult.Cancel:
+                    return Result.Cancelled;
+                //Unexpected result
+                default:
+                    return Result.Failed;
+            }
+            log.AppendLine("Output directory selected: {0}", destinationDirectory);
+
+            familiesToMigrate = GetEligibleFamiles(sourceDirectory);
+            log.AppendLine("{0} elligible families found", familiesToMigrate.Count());
+            foreach (var family in familiesToMigrate)
+            {
+                log.AppendLine("  {0}", family.SourceFileName);
+            }
 
             String logFilePath = FileLocations.AddInDirectory + String.Format("{0}.log.txt", DateTime.Now.ToString("yyyyddMM.Hmm"));
             File.WriteAllText(logFilePath, log.Text);
             return Result.Succeeded;
+        }
+
+        /// <summary>
+        /// Return all familes elligible for upgrade in a specified directory. The directory is searched recursively
+        /// </summary>
+        /// <param name="sourceDirectory">The directory to search</param>
+        /// <returns>A List of families found in the directory</returns>
+        private List<RevitFamily> GetEligibleFamiles(String sourceDirectory)
+        {
+            List<RevitFamily> selectedFamiles = new List<RevitFamily>();
+
+            List<String> filePaths = Directory.EnumerateFiles(sourceDirectory, "*.rfa", SearchOption.AllDirectories).ToList();
+            foreach (var path in filePaths)
+            {
+                selectedFamiles.Add(new RevitFamily(path));
+            }
+
+            return selectedFamiles;
         }
     }
 
---
+++ Revit-Migration/Models/RevitFamily.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DougKlassen.Revit.Migration.Models
+{
+    /// <summary>
+    /// A Revit family
+    /// </summary>
+    public class RevitFamily
+    {
+        /// <summary>
+        /// The path of the directory of the .rfa file containing the family. The trailing backslash is included"
+        /// </summary>
+        public String SourceFilePath;
+
+        /// <summary>
+        /// The file name of the .rfa file containing the family
+        /// </summary>
+        public String SourceFileName;
+
+        /// <summary>
+        /// The complete path of the .rfa file containing the family
+        /// </summary>
+        public String SourceFileCompletePath
+        {
+            get
+            {
+                return SourceFilePath + SourceFileName;
+            }
+        }
+
+        /// <summary>
+        /// Construct a RevitFamily representing a specified .rfa file
+        /// </summary>
+        /// <param name="path">The path to the .rfa file containing the family</param>
+        public RevitFamily(String path)
+        {
+            SourceFilePath = Path.GetDirectoryName(path);
+            SourceFileName = Path.GetFileName(path);
+        }
+    }
+}
